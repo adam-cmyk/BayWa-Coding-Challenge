ca91b3613aa6197ddbb521672824060f0f7eb0f1
adam-cmyk <riedel.a@mail.de>
2023-02-17 11:28:43 +0100

    Bug Fixes

--- 2022-12-31_Bewerbung-Studium.py
+++ 2022-12-31_Bewerbung-Studium.py
@@ -1,6 +1,5 @@
 from random import * #um eine Zufallszahl generieren zu können
-from getpass import getpass #Passwortmodul in Python --> Schriftfarbe wird nicht angezeigt
-from re import S #reguläre Ausdrücke
+from getpass import getpass #Passwortmodul in Python --> Schriftfarbe wird nicht angezeigta
 from time import sleep #um Zeiten zwischen einzelnen Schritten zu ermöglichen
 
 f"bcolors.RESET_COLOR"
@@ -18,29 +17,35 @@ class bcolors:
 print("Willkommen zu einem kleinen lustigen Zahlenratenspiel für zwei Spieler :) \nViel Spaß :) ") 
 print("Das Spiel funktioniert wie folgt: Es werden zufällige Zahlen (von 0 bis 250) vom Computer ausgesucht, dabei soll jeder Spieler versuchen, so genau wie möglich, an der ausgesuchten Zahl zu sein. \nACHTUNG: Erlaubt sind nur ganze natürliche Zahlen!") # kurze Info über das Spiel
 
-player_number = 2 
+player_number = 2
 b = 0
+dis1 = 1
+dis2 = 1
+points_player2 = 0
+points_player1 = 0
+
 while b == 0:
-    if player_number == 2:
-        player_1 = input("Name des 1. Spielers?: ") #Namenseingabe des 1. Spieler
-        player_2 = input("Name des 2. Spielers?: ") #Namenseingabe des 2. Spieler
-        if player_1 == player_2:
-                print("Fehler. Beide Spieler haben denselben Namen. Bitte wiederholen und unterscheidliche Namen verwenden.")
-                break
-    else:
-        print("Fehler. Es wurden zu viele Spieler bzw. keine zwei Spieler angefragt. \nBitte den Vorgang wiederholen")
-        break
-        
     rounds = int(input("Wie viele Runden soll gespielt werden?: ")) #Nachfrage, wie oft gespielt werden soll
     if rounds == 0:
         print("Fehler. Es wurde nicht für eine Runde gestartet") # Fehlermeldung falls keine Runden eingegeben wurden
         break
+    if player_number == 2:
+        player_1 = input("Name des 1. Spielers?: ") #Namenseingabe des 1. Spieler
+        player_2 = input("Name des 2. Spielers?: ") #Namenseingabe des 2. Spieler
+        if player_1 == player_2:
+            print("Fehler. Beide Spieler haben denselben Namen. Bitte wiederholen und unterscheidliche Namen verwenden.")
+            break
+    else:
+        print("Fehler. Es wurden zu viele Spieler bzw. keine zwei Spieler angefragt. \nBitte den Vorgang wiederholen")
+        break
+        
+    
         
     i = 0
     while i < rounds:
         i += 1
-        r = randint(0, 250) #Zufallszahl zwischen 0 und 250
-        x = int(randint(1, r))
+        x = randint(0, 250) #Zufallszahl zwischen 0 und 250
+        #x = int(randint(1, r))
     
         if player_number == 2:
             input_player1 = int(getpass("Zahl von %s : "%(player_1))) #verdeckte Zahleingabe der Spieler
@@ -72,37 +77,37 @@ while b == 0:
         else:
             print("Ein Fehler ist aufgetreten. Bitte noch einmal versuchen zu spielen.")
 
-        points_player2 = 0
-        points_player1 = 0
 
-        if dis1 > dis2: # wenn Spieler 1 näher an der gesuchten Zahl ist
+        if dis1 > dis2: # wenn Spieler 2 näher an der gesuchten Zahl ist
             print(f"{bcolors.GREEN}%s erhält einen Punkt{bcolors.RESET_COLOR}"%(player_2))
             points_player2 += 1
+            if dis2 == x:
+                print("Glückwunsch die richtige Zahl wurde getroffen. Du erhältst 2 Punkte")
+                points_player2 += 1
         elif dis1 == dis2: # wenn beide Spieler dieselbe Zahl haben
             print(f"{bcolors.YELLOW}%s hat die gleiche Zahl wie %s {bcolors.RESET_COLOR}"%(player_1, player_2),". Somit erhält keiner der beiden Spieler einen Punkt.")
-        else: # wenn Spieler 2 näher an der geuschten Zahl ist
+        else: # wenn Spieler 1 näher an der geuschten Zahl ist
             print(f"{bcolors.GREEN}%s erhält einen Punkt{bcolors.RESET_COLOR}"%(player_1))
             points_player1 += 1
+            if dis1 == x:
+                print("Glückwunsch die richtige Zahl wurde getroffen. Du erhälst 2 Punkte")
+                points_player1 += 1
         print("Zwischenstand: ", player_1, ":", points_player1, " Punkt(e) ;", player_2, ":", points_player2, "Punkt(e)")
     break
 
 sleep(1)
-points_player_2 = 0
-points_player_1 = 0
-while rounds > 0:
-    rounds -= 1
-    if dis1 > dis2:
-        points_player_2 += 1
-    elif dis1 == dis2:
-        break
-    else:
-        points_player_1 += 1
-sleep(1)
-print("Endpunktestand: ", player_1, ":", points_player_1, " Punkte ;", player_2, ":", points_player_2, "Punkte")
-print("Herzlichen Glückwunsch. Ihr habt das Spiel beendet")
+
+print("Herzlichen Glückwunsch. Ihr habt das Spiel beendet.")
+if dis1 > dis2:
+    print(f"{bcolors.YELLOW}%s hat das Spiel gewonnen." %(player_2))
+elif dis2 == dis1:
+    print(f"{bcolors.YELLOW} Gleichstand. Ihr beiden seid zusammen Gewinner!")
+else:
+    print(f"{bcolors.YELLOW}%s hat das Spiel gewonnen." %(player_1))
       
-retry = int(input("Möchtet ihr nochmal spielen (0 = ja, 1 = nein)? "))
+retry = int(input(f"{bcolors.RESET_COLOR} Möchtet ihr nochmal spielen (0 = ja, 1 = nein)? "))
 if retry == 0:
+    x = randint(0, 250)
     counter_repeat = int(input("Wie oft wollt ihr das Spiel wiederholen?: "))
     while counter_repeat > 0:
         counter_repeat -= 1
